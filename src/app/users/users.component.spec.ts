304e8c2263a478d8e185a9b7246903471ef007bd
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-05 19:27:24 +0530

    updated

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { UsersComponent } from './users.component';
 
@@ -22,4 +22,42 @@ describe('UsersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have isLoading true by default', () => {
+    expect(component.isLoading).toBeTrue();
+  });
+
+  // Approach #1 [RECOMMENDED]
+  it('has London as city - Approach #1', fakeAsync(() => {
+    component.ngOnInit();
+    tick(4001);
+    expect(component.isLoading).toBeFalse();
+    expect(component.myCity).toEqual('London');
+  }));
+
+  // Approach #2 
+  it('has London as city - Approach #2', ((done) => {
+    component.ngOnInit();
+
+    setTimeout( () => {
+      expect(component.isLoading).toBeFalse();
+      expect(component.myCity).toEqual('London');
+      done();
+    }, 4001);
+  }));
+
+  it('should have 2 elements in userList array', fakeAsync(() => {
+    component.ngOnInit();
+    tick(4001);
+    expect(component.userList.length).toEqual(2);
+  }));
+
+  it('should render atleast one user in HTML', fakeAsync(() => {
+    component.ngOnInit();
+    tick(4001);
+
+    fixture.detectChanges();  // to get the updated html after initial rendering
+    const userNameEl = fixture.nativeElement.querySelector('.userName');
+    expect(userNameEl?.innerText).toContain('John Williams');
+  }));
 });
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  isLoading = true;
+  myCity = '';
+  userList: any[] = [];
+
+  constructor() { 
+    console.log('Inside Constructor');
+  }
 
   ngOnInit(): void {
+    console.log('Inside ngOnInit');
+    // whenever the comp is coming into view -- ngOnInit will be called
+    // ideal lifecycle hook for us to connet with REST API 
+    
+    
+
+    // faking ajax call
+    setTimeout( () => {
+      this.isLoading = false;
+      this.myCity = 'London';
+
+      // filling this with fake data. 
+      this.userList = [
+        {
+          id: 1, 
+          name: 'John Williams',
+          phone: 3434232
+        },
+        {
+          id: 2, 
+          name: 'Steve Smith',
+          phone: 56756674
+        }
+      ];
+      
+    }, 4000);
   }
 
 }
